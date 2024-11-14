836e4b490862712897ff7fcc0f35024d878f01a3
Lucas Noel <129121307+lucasnoelgb@users.noreply.github.com>
2024-11-14 13:32:03 -0300

    teste

--- adam_adamowicz/lib/main.dart
+++ adam_adamowicz/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(AdamAdamowiczApp());
@@ -27,8 +28,7 @@ class HomeScreen extends StatelessWidget {
           'Adam Adamowicz',
           style: TextStyle(color: Colors.purple[100]), // Letra roxa clara
         ),
-        iconTheme:
-            IconThemeData(color: Colors.white), // Cor da seta para branco
+        iconTheme: IconThemeData(color: Colors.white), // Cor da seta para branco
       ),
       body: Container(
         color: Colors.purple[100], // Fundo roxo claro
@@ -72,6 +72,14 @@ class HomeScreen extends StatelessWidget {
 }
 
 class CompanyScreen extends StatelessWidget {
+  final Uri _companyUrl = Uri.parse('https://www.bethesda.net/'); // URL da empresa
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_companyUrl)) {
+      throw 'Could not launch $_companyUrl';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,17 +89,36 @@ class CompanyScreen extends StatelessWidget {
           'Empresa',
           style: TextStyle(color: Colors.purple[100]), // Letra roxa clara
         ),
-        iconTheme:
-            IconThemeData(color: Colors.white), // Cor da seta para branco
+        iconTheme: IconThemeData(color: Colors.white), // Cor da seta para branco
       ),
       body: Container(
         color: Colors.purple[100], // Fundo roxo claro
-        child: Center(
-          child: Text(
-            'Adam Adamowicz trabalhou na Bethesda Softworks, uma das principais desenvolvedoras de jogos.',
-            style: TextStyle(fontSize: 18),
-            textAlign: TextAlign.center,
-          ),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Bethesda Softworks',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Bethesda Softworks é uma das principais desenvolvedoras e publicadoras de jogos do mundo, conhecida por sua contribuição para a indústria de jogos eletrônicos desde sua fundação em 1986. A empresa tem um histórico de inovações tecnológicas e narrativas, sendo pioneira na criação de jogos que moldaram a indústria moderna.'
+              '\n\nA Bethesda é amplamente reconhecida por franquias de sucesso, como Fallout e The Elder Scrolls, ambas aclamadas por suas vastas histórias, mundos abertos expansivos e jogabilidade envolvente. Fallout 3 e The Elder Scrolls V: Skyrim, especialmente, elevaram o padrão de RPGs no cenário global.'
+              '\n\nAo longo dos anos, a empresa continuou a crescer, adquirindo estúdios renomados e expandindo seu portfólio de jogos. Com uma cultura que valoriza a criatividade e inovação, Bethesda Softworks mantém-se como uma das líderes no desenvolvimento de jogos AAA, sempre focada em oferecer experiências imersivas e memoráveis aos jogadores.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: _launchUrl,
+                child: Text('Visite o site da empresa'),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -108,8 +135,7 @@ class GamesScreen extends StatelessWidget {
           'Jogos Produzidos',
           style: TextStyle(color: Colors.purple[100]), // Letra roxa clara
         ),
-        iconTheme:
-            IconThemeData(color: Colors.white), // Cor da seta para branco
+        iconTheme: IconThemeData(color: Colors.white), // Cor da seta para branco
       ),
       body: Container(
         color: Colors.purple[100], // Fundo roxo claro
@@ -146,8 +172,7 @@ class AboutScreen extends StatelessWidget {
           'Sobre Adam Adamowicz',
           style: TextStyle(color: Colors.purple[100]), // Letra roxa clara
         ),
-        iconTheme:
-            IconThemeData(color: Colors.white), // Cor da seta para branco
+        iconTheme: IconThemeData(color: Colors.white), // Cor da seta para branco
       ),
       body: Container(
         color: Colors.purple[100], // Fundo roxo claro
