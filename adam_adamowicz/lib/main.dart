1f254271304fb96fa36c0bbedc53603a197b9584
Lucas Noel <129121307+lucasnoelgb@users.noreply.github.com>
2024-11-14 16:02:02 -0300

    commits

--- adam_adamowicz/lib/main.dart
+++ adam_adamowicz/lib/main.dart
@@ -85,10 +85,6 @@ class CompanyScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text(
-          'Empresa',
-          style: TextStyle(color: Colors.purple[100]), // Letra roxa clara
-        ),
         iconTheme: IconThemeData(color: Colors.white), // Cor da seta para branco
       ),
       body: Container(
@@ -96,19 +92,20 @@ class CompanyScreen extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              'Bethesda Softworks',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+            Center(
+              child: Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Bethesda_Game_Studios_logo.svg/1200px-Bethesda_Game_Studios_logo.svg.png',
+                height: 150,
+              ),
             ),
             SizedBox(height: 20),
             Text(
               'Bethesda Softworks é uma das principais desenvolvedoras e publicadoras de jogos do mundo, conhecida por sua contribuição para a indústria de jogos eletrônicos desde sua fundação em 1986. A empresa tem um histórico de inovações tecnológicas e narrativas, sendo pioneira na criação de jogos que moldaram a indústria moderna.'
               '\n\nA Bethesda é amplamente reconhecida por franquias de sucesso, como Fallout e The Elder Scrolls, ambas aclamadas por suas vastas histórias, mundos abertos expansivos e jogabilidade envolvente. Fallout 3 e The Elder Scrolls V: Skyrim, especialmente, elevaram o padrão de RPGs no cenário global.'
               '\n\nAo longo dos anos, a empresa continuou a crescer, adquirindo estúdios renomados e expandindo seu portfólio de jogos. Com uma cultura que valoriza a criatividade e inovação, Bethesda Softworks mantém-se como uma das líderes no desenvolvimento de jogos AAA, sempre focada em oferecer experiências imersivas e memoráveis aos jogadores.',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(fontSize: 14), // Tamanho da fonte menor
               textAlign: TextAlign.justify,
             ),
             SizedBox(height: 20),
